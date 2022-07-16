179a061cb103c257f1be72b46ef62968df2ee6b3
raza <coder.aliraza@gmail.com>
2022-07-16 20:55:52 +0500

    finaaly done

---
+++ js/app.js
@@ -0,0 +1,11 @@
+const btn = document.getElementById('menu-btn')
+const nav = document.getElementById('menu')
+
+if(btn){
+    btn.addEventListener('click', () => {
+        btn.classList.toggle('open')
+        nav.classList.toggle('flex')
+        nav.classList.toggle('hidden')
+    })
+}
+
