73295136814740bdea240053c95ca50e8c3ec7a6
raza <coder.aliraza@gmail.com>
2022-07-30 23:04:04 +0500

    t

--- js/app.js
+++ js/app.js
@@ -9,3 +9,45 @@ if(btn){
     })
 }
 
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("on-slide");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+    }
+    slides[slideIndex-1].style.display = "block";
+}
+
+
+function showSlide2(n) {
+    var i;
+    var slides = document.getElementsByClassName("sc-slide");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+    }
+    slides[slideIndex-1].style.display = "block";
+}
+
+function plusSlides2(n) {
+    showSlide2(slideIndex += n);
+}
+function currentSlide2(n) {
+    showSlide2(slideIndex = n);
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+
+var slideIndex2 = 1;
+showSlide2(slideIndex2);
--- tailwind.config.js
+++ tailwind.config.js
@@ -20,6 +20,8 @@ module.exports = {
       // => @media (min-width: 1536px) { ... }
     }
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
 
